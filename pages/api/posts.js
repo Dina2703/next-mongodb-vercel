281f6edc0595e27531b2fd505f317ca3ce9e3ee4
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-16 21:44:29 -0400

    Fetching posts

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -21,3 +21,27 @@ export default async function handler(req, res) {
     }
   }
 }
+
+async function getPosts(req, res) {
+  try {
+    // connect to the database
+    let { db } = await connectToDatabase();
+    // fetch the posts
+    let posts = await db
+      .collection("posts")
+      .find({})
+      .sort({ published: -1 })
+      .toArray();
+    // return the posts
+    return res.json({
+      message: JSON.parse(JSON.stringify(posts)),
+      success: true,
+    });
+  } catch (error) {
+    // return the error
+    return res.json({
+      message: new Error(error).message,
+      success: false,
+    });
+  }
+}
