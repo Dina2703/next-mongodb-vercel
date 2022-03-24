259f5305437449f916212ce382f375b2d7f208c9
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-23 23:49:38 -0400

    adding "Content-Type": "application/json"

--- pages/add-post.js
+++ pages/add-post.js
@@ -30,7 +30,7 @@ export default function AddPost() {
     let response = await fetch("/api/posts", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json;charset=utf-8",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(post),
     });
